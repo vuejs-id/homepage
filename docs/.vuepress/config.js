682e9cbdb91bcf47451edc0736847657ee14dcf8
Mulia Arifandi Nasution <mul14.net@gmail.com>
2018-06-04 07:05:02 +0700

    Update configs
    Signed-off-by: Mulia Arifandi Nasution <mul14.net@gmail.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,18 @@
 module.exports = {
   title: 'Vue.js Indonesia',
-  description: '',
+  description: 'Vue.js Indonesian Community',
+  ga: 'UA-82114244-1',
   themeConfig: {
+    search: false,
+    lastUpdated: false,
+    repo: 'vuejs-id',
+    docsRepo: 'vuejs-id/homepage',
+    docsDir: 'docs',
+    editLinks: false,
+    editLinkText: 'Improve this page',
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'Meetup', link: '/meetup/' },
       { text: 'Tutorial',
         items: [
           { text: 'Medium', link: 'https://medium.com/vuejs-id' },
@@ -14,7 +23,6 @@ module.exports = {
           { text: 'Facebook', link: 'https://www.facebook.com/groups/1675298779418239/' },
           { text: 'Telegram', link: 'https://t.me/vuejsid' }
       ]},
-      { text: 'GitHub', link: 'https://github.com/vuejs-id' },
     ]
   }
 }
